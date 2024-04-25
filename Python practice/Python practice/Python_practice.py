899ca86343a7282b4f81c04dc73e04470bd63563
Osama Hussein <osamahusseinn2@gmail.com>
2024-04-25 16:09:15 -0400

    I learned how to use the print function inside of Python, and I also used a conditional to print the string to the console!

---
+++ Python practice/Python practice/Python_practice.py
@@ -0,0 +1,8 @@
+# I learned some Python code here: https://www.learnpython.org/
+
+print("My name is Osama Hussein and I am practicing writing this script in Python language, while learning more about Python on the go!")
+
+r = 2;
+
+if r == 2:
+    print("r is equal to 2!");
