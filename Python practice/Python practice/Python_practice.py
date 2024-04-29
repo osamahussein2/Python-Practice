fc899690a18a914c7eee079ddeb17a184d4f76b1
Osama Hussein <osamahusseinn2@gmail.com>
2024-04-29 13:51:18 -0400

    I learned how to use variables and variable types inside of Python

--- Python practice/Python practice/Python_practice.py
+++ Python practice/Python practice/Python_practice.py
@@ -6,3 +6,41 @@ r = 2;
 
 if r == 2:
     print("r is equal to 2!");
+
+integerValue = 9;
+print(integerValue);
+
+floatValue = 9.2;
+print(floatValue);
+
+floatValue2 = float(9.55);
+print(floatValue2);
+
+hi = 'hi';
+print(hi);
+
+helloThere = "hello there"
+print(helloThere);
+
+# So using double quotations inside a string makes it easier to this program that apostrophes are included
+# inside the string compared to single quotations where using apostrophes would not be detected inside the string
+
+six = 6;
+four = 4;
+sumOfNumbers = four + six;
+
+print(sumOfNumbers);
+
+welcome = "Welcome";
+to = "to";
+python = "the Python language!"
+
+pythonString = welcome + " " + to + " " + python;
+print(pythonString);
+
+two, four = 2, 4;
+print(two, four);
+
+# If there is a number variable and a string inside the print function, the program will print an error since
+# the program cannot convert a number variable to a string, and vice versa
+
